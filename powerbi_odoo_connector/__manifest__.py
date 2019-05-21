e82eede445eebb545adc6de422f3622c760ae4bd
Fernando <46256429+GanemoCorp@users.noreply.github.com>
2019-05-21 13:42:57 -0500

    [ADD] app powerbi_odoo_connector

---
+++ powerbi_odoo_connector/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PowerBI odoo connector",
+
+    'description': """
+        Need help in visualizing your Odoo data with a powerful tool such as Power BI? Check out the description and feel free to get in touch with us for implementation of the same.
+    """,
+
+    'author': "Techneith",
+    'website': "info@techneith.com",
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/resources.xml',
+    ],
+    'price': 0,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
+
+    'demo': [
+        #'demo/demo.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
